ee53faacf278c602a49e05f5ce44a36de0900478
AnandJoy7 <k.anad548@gmail.com>
2024-02-28 19:19:16 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import Home from "./Page/Home";
+import About from "./Page/About";
+import Todo from "./Page/Todo";
+import Login from "./Auth/Login";
+import Signup from "./Auth/Signup";
+import Navbar from "./Page/Navbar";
+import Contact from "./Page/Contact";
+import Subscribe from "./Page/Subscribe";
+import "./Styles/Home.css";
+import "./Styles/Todo.css";
+import "./Styles/Auth.css";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+
+
+  return (
+    <>
+    <Navbar />
+
+<Routes>
+  <Route path="/home" element={<Home />}></Route>
+  <Route path="/aboutus" element={<About />}></Route>
+  <Route path="/todo" element={<Todo />}></Route>
+  <Route path="/subscribe" element={<Subscribe />}></Route>
+  <Route path="/contactus" element={<Contact />}></Route>
+  <Route path="/signin" element={<Login />}></Route>
+  <Route path="/signup" element={<Signup />}></Route>
+</Routes>
+
+    </>
+  )
+}
+
+export default App
